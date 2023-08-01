2b23f2016cde4e6294043a51a6a80e35f9213443
Amezaiku <52254100+DesertDragen@users.noreply.github.com>
2023-08-01 16:26:35 -0400

    Create void_function.cpp
    Learned what a void modifier on a function does. It return no value. Used if you want something to be printed into the terminal.
    
    Learned how to write and call a function.

---
+++ functions/void_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// void has no return value- want to use this if you want to just print something
+void oscar_wilde_quote() {
+
+    std::cout << "The highest, as the lowest, form of criticism is a mode of autobiography.";
+
+}
+
+int main() {
+
+    oscar_wilde_quote();
+
+}
