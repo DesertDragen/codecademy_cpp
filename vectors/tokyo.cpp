a9f59ee2b30dfcc43bf94fa368dd548e75970144
Amezaiku <52254100+DesertDragen@users.noreply.github.com>
2023-08-01 15:07:32 -0400

    Update tokyo.cpp
    Added a line where it initializes the vector with a size.

--- vectors/tokyo.cpp
+++ vectors/tokyo.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::vector<double> subway_adult = {800, 1200, 1500};
     std::vector<double> subway_child = {400, 600, 750};
+    std::vector<double> subway_senior(3);
 
     std::cout << subway_child[2];
 }
