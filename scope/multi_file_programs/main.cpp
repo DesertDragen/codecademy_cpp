c255bff95ad1c4e88cb82292296e6fb87e0d4044
Amezaiku <52254100+DesertDragen@users.noreply.github.com>
2023-08-04 11:18:23 -0400

    Created 2 folders and 3 files
    Created scope folder and multi_file_programs
    
    Created main.cpp, my_functions.cpp and my_functions.h
    
    Added code from Codecademy

---
+++ scope/multi_file_programs/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include "my_functions.h"
+
+
+int main() {
+
+    std::cout << is_palindrome("racecar") << "\n";
+    std::cout << tenth_power(3) << "\n";
+    std::cout << average(8.0, 19.0) << "\n";
+
+}
---
+++ scope/multi_file_programs/my_functions.h
@@ -0,0 +1,10 @@
+#ifndef MY_FUNCTIONS_H
+#define MY_FUNCTIONS_H
+
+#include <string>
+
+double average(double num1, double num2);
+int tenth_power(int num);
+bool is_palindrome(std::string text);
+
+#endif
